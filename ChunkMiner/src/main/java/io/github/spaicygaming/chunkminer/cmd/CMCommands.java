c444ddf657b681e4152cb87c8c16e58c92e45689
SpaicyGaming <spaicygraphic@gmail.com>
2018-03-27 19:04:57 +0200

    allow the command "/miner give" to be executed non in-game

--- ChunkMiner/src/main/java/io/github/spaicygaming/chunkminer/cmd/CMCommands.java
+++ ChunkMiner/src/main/java/io/github/spaicygaming/chunkminer/cmd/CMCommands.java
@@ -14,17 +14,17 @@ public class CMCommands implements CommandExecutor {
 	private ChunkMiner main = ChunkMiner.getInstance();
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String alias, String[] args) {
-
-		if (!(sender instanceof Player)) {
-			sender.sendMessage("This command can be executed only by a player");
-			return false;
-		}
-		Player player = (Player) sender;
 		
 		// Length 2
 		if (args.length == 2) {
 			// Get
 			if (args[0].equalsIgnoreCase("get")) {
+				if (!(sender instanceof Player)) {
+					sender.sendMessage(ChatUtil.c("onlyInGame"));
+					return false;
+				}
+				Player player = (Player) sender;
+				
 				if (!player.hasPermission(Const.PERM_GET)) {
 					player.sendMessage(ChatUtil.c("noCmdPerms"));
 					return false;
@@ -47,7 +47,7 @@ public class CMCommands implements CommandExecutor {
 			}
 			// Wrong args
 			else {
-				printHelpMenu(player);
+				printHelpMenu(sender);
 			}
 			
 			return false;
@@ -58,8 +58,8 @@ public class CMCommands implements CommandExecutor {
 			// Give
 			if (args[0].equalsIgnoreCase("give")) {
 				// Check permission
-				if (!player.hasPermission(Const.PERM_GIVE)) {
-					player.sendMessage(ChatUtil.c("noCmdPerms"));
+				if (!sender.hasPermission(Const.PERM_GIVE)) {
+					sender.sendMessage(ChatUtil.c("noCmdPerms"));
 					return false;
 				}
 			
@@ -67,20 +67,20 @@ public class CMCommands implements CommandExecutor {
 				// Controlla se il target è offline
 				Player target = main.getServer().getPlayer(targetName);
 				if (target == null) {
-					player.sendMessage(ChatUtil.c("targetOffline").replace("{target}", targetName));
+					sender.sendMessage(ChatUtil.c("targetOffline").replace("{target}", targetName));
 					return false;
 				}
 				
 				// Controlla che l'amount sia un numero valido
 				if (!validInt(args[2])) {
-					player.sendMessage(ChatUtil.c("invalidAmount"));
+					sender.sendMessage(ChatUtil.c("invalidAmount"));
 					return false;
 				}
 				int amount = Integer.valueOf(args[2]);
 				
 				// Controlla se c'è spazio nell'inventario
 				if (target.getInventory().firstEmpty() == -1) {
-					player.sendMessage(ChatUtil.c("targetFullInventory").replace("{target}", targetName));
+					sender.sendMessage(ChatUtil.c("targetFullInventory").replace("{target}", targetName));
 					return false;
 				}
 				
@@ -88,21 +88,21 @@ public class CMCommands implements CommandExecutor {
 				main.getMinerItem().give(target, amount);
 				
 				// Messages
-				player.sendMessage(ChatUtil.c("itemGived").replace("{target}", targetName)
+				sender.sendMessage(ChatUtil.c("itemGived").replace("{target}", targetName)
 						.replace("{amount}", String.valueOf(amount)));
-				target.sendMessage(ChatUtil.c("itemReceivedOther").replace("{giver}", player.getName())
+				target.sendMessage(ChatUtil.c("itemReceivedOther").replace("{giver}", sender.getName())
 						.replace("{amount}", String.valueOf(amount)));
 				
 				return true;
 			} else {
-				printHelpMenu(player);
+				printHelpMenu(sender);
 			}
 			
 		}
 		
 		// Invalid Length
 		else {
-			printHelpMenu(player);
+			printHelpMenu(sender);
 		}
 		
 		return false;
